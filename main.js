a65a9fb3b68d6d279c760a9ff89836101246a81a
Tirsa St. Fort <lifelovelori@gmail.com>
2023-02-17 07:49:38 -0800

    Completed lab

--- main.js
+++ main.js
@@ -10,20 +10,18 @@ const hearts = document.getElementsByClassName("like-glyph")
 
 for (const heart of hearts) {
   heart.addEventListener("click", (e) => {
-    if (e.target.innerHTML = EMPTY_HEART) {
+    if (e.target.innerHTML === EMPTY_HEART) {
       mimicServerCall()
       .then((resolve) => e.target.innerHTML = FULL_HEART)
       .then((resolve) => e.target.className = "activated-heart")
       .catch((error) => document.getElementById("modal").classList.remove("hidden"))
       setTimeout(() => document.getElementById("modal").className = "hidden", 3000)
-      console.log(hearts)
-    } else if (e.target.innerHTML = FULL_HEART) {
-      console.log(e)
+    } else if (e.target.innerHTML === FULL_HEART) {
+      e.target.innerHTML = EMPTY_HEART
+      e.target.classList.remove("activated-heart")
     }
   })
 }
-
-
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
